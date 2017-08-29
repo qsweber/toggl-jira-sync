7296f034ccd3c03db21121b758ff0c3639c12f30
Quinn Weber <quinn@quinnweber.com>
2017-08-29 09:45:14 -0700

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='toggl_jira_sync',
+    name='toggl-jira-sync',
     packages=['toggl_jira_sync'],
     description='Sync Toggl with JIRA',
     version='0.0.2',
     author='Quinn Weber',
+    author_email='quinn@quinnweber.com',
     maintainer='Quinn Weber',
     maintainer_email='quinn@quinnweber.com',
     url='https://github.com/qsweber/toggl-jira-sync',
-    download_url='https://github.com/qsweber/toggl-jira-sync/archive/v0.0.2.zip',
     install_requires=(
         'requests',
         'python-dateutil',
